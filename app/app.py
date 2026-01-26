b88aad0272e9940d39ee068ffe6b6c01e0e9469e
SeyedRumaiz <mrsrumaiz2007@gmail.com>
2026-01-26 18:25:36 +0530

    Added external CSS file

--- app/app.py
+++ app/app.py
@@ -10,3 +10,8 @@ if "predicted" not in st.session_state:
     st.session_state.uploaded_file = False
     st.session_state.result = None
     st.session_state.confidence = None
+
+# Use the CSS file
+with open("styles.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
