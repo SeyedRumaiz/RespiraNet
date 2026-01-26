44611efdfbd9042ddc9f9c22039a96ae3ab4aee2
SeyedRumaiz <mrsrumaiz2007@gmail.com>
2026-01-26 18:58:08 +0530

    Added temporary session states

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from model import classify
 
 # Setup config
-st.set_page_config(page_title='RespiraNet', layout='wide')
+st.set_page_config(page_title='RespiraNet', layout='wide')  # page_icon=""
 
 # Initialize session state
 if "predicted" not in st.session_state:
@@ -10,6 +10,8 @@ if "predicted" not in st.session_state:
     st.session_state.uploaded_file = False
     st.session_state.result = None
     st.session_state.confidence = None
+    st.session_state.accuracy = None
+    st.session_state.location = None
 
 # Use the CSS file
 with open("styles.css") as f:
